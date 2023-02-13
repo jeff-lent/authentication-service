ca86649bf4811489277120a385f2e1942f4e3c64
Hunain Parekh <hunain.parekh@xloopdigital.com>
2023-02-13 17:35:11 +0500

    cross origin add

--- src/main/java/com/xloop/resourceloop/authenticationservice/Controller/UserController.java
+++ src/main/java/com/xloop/resourceloop/authenticationservice/Controller/UserController.java
@@ -4,6 +4,7 @@ package com.xloop.resourceloop.authenticationservice.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.xloop.resourceloop.authenticationservice.JPARepository.UserRepository
 import com.xloop.resourceloop.authenticationservice.Model.User;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/user")
 public class UserController {
     @Autowired
